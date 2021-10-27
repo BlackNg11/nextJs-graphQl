db241ecad16c995816b08abf9408981b35453933
Black Nguyen <blackng10.5.1997@gmail.com>
2021-10-28 02:56:49 +0700

    update

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,4 +1,6 @@
 import Navbar from '../components/Navbar';
+import { addApolloState, initializeApollo } from '../lib/apolloClient';
+import { PostsDocument } from "../generated/graphql";
 
 const index = () => {
   return (
@@ -7,6 +9,18 @@ const index = () => {
       <h1>Hello World</h1>
     </>
   );
+}
+
+export const getStaticProps = async () => {
+  const apolloClient = initializeApollo()
+
+  await apolloClient.query({
+    query: PostsDocument,
+  })
+
+  return addApolloState(apolloClient, {
+    props: {},
+  })
 }
 
 export default index;
