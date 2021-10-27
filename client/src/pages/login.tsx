fd2cabce082e63560459eddf86351a9783b106c2
Black Nguyen <blackng10.5.1997@gmail.com>
2021-10-27 16:06:55 +0700

    login

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -4,31 +4,30 @@ import { useRouter } from 'next/router';
 
 import InputField from "../components/InputField";
 import Wrapper from "../components/Wrapper";
-import { RegisterInput, useRegisterMutation } from "../generated/graphql";
+import { LoginInput, useLoginMutation } from "../generated/graphql";
 import { mapFieldsErrors } from "../helper/mapFieldsErrors";
 
 
 const Login = () => {
   const router = useRouter()
 
-  const initialValues = {
-    username: '',
-    email: '',
+  const initialValues: LoginInput = {
+    usernameOrEmail: '',
     password: ''
   }
 
-  const [registerUser, { loading: _registerUserLoading, data, error }] = useRegisterMutation()
+  const [loginUser, { loading: _loginUserLoading, data, error }] = useLoginMutation()
 
-  const onRegisterSubmit = async (values: RegisterInput, { setErrors }: FormikHelpers<RegisterInput>) => {
-    const response = await registerUser({
+  const onLoginSubmit = async (values: LoginInput, { setErrors }: FormikHelpers<LoginInput>) => {
+    const response = await loginUser({
       variables: {
-        registerInput: values
+        loginInput: values
       }
     })
 
-    if (response?.data?.register?.errors) {
-      setErrors(mapFieldsErrors(response?.data?.register?.errors))
-    } else if (response?.data?.register?.user) {
+    if (response?.data?.login?.errors) {
+      setErrors(mapFieldsErrors(response?.data?.login?.errors))
+    } else if (response?.data?.login?.user) {
       router.push('/')
     }
 
@@ -37,26 +36,21 @@ const Login = () => {
   return (
     <Wrapper>
       {error && <p>Failed to register</p>}
-      {data && data.register.success && <p>Registered successfully</p>}
-      <Formik initialValues={initialValues} onSubmit={onRegisterSubmit}>
+      {data && data.login.success && <p>Login successfully</p>}
+      <Formik initialValues={initialValues} onSubmit={onLoginSubmit}>
         {
           ({ isSubmitting }) => (
             <Form>
               <FormControl>
-                <InputField name="username" label="Username" placeholder="Username" type="text" />
+                <InputField name="usernameOrEmail" label="Username or Email" placeholder="Username or Email" type="text" />
               </FormControl>
-              <Box mt={4}>
-                <FormControl>
-                  <InputField name="email" label="Email" placeholder="Email" type="text" />
-                </FormControl>
-              </Box>
               <Box mt={4}>
                 <FormControl>
                   <InputField name="password" label="Password" placeholder="Password" type="password" />
                 </FormControl>
               </Box>
               <Button type="submit" colorScheme="teal" mt={4} isLoading={isSubmitting}>
-                Register
+                Login
               </Button>
             </Form>
           )
